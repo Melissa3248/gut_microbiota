475a6e2c5a081efc6567b4d12ad34ea48bd5e011
Melissa <{ID}+{username}@users.noreply.github.com>
2023-10-20 19:44:27 -0700

    added a line to create a save directory if it doesnt exit

--- parallel_jobs/param_set1837.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -C cpu
-#SBATCH -q regular # this is probably named something different on the iowa cluster
-#SBATCH -J param_set1837
-#SBATCH -t 00:15:00
-#SBATCH --output=parallel_jobs/out_files/%x.out
-julia generate_params.jl 512 1837 /pscratch/sd/m/maadrian/testing_saving
